32c16ee30c021611b3c9808c9f46f9c9ba3b0b91
Intranet2023 <151795189+Intranet2023@users.noreply.github.com>
2023-11-27 09:46:32 -0500

    Update GitSleuth_API.py

--- GitSleuth_API.py
+++ GitSleuth_API.py
@@ -1,5 +1,3 @@
-#GitSleuth_API
-import time
 import requests
 import base64
 import logging
@@ -10,10 +8,12 @@ GITHUB_API_URL = 'https://api.github.com/'
 def handle_api_response(response):
     """
     Handles the API response, checking for errors and logging appropriately.
+
     Parameters:
-    response (Response): The response object from requests library.
+    - response (requests.Response): The response object from requests library.
+
     Returns:
-    dict or None: Parsed JSON response if the request was successful, None otherwise.
+    - dict or None: Parsed JSON response if the request was successful, None otherwise.
     """
     if response.status_code == 200:
         return response.json()
@@ -24,12 +24,14 @@ def handle_api_response(response):
 def fetch_paginated_data(url, headers, max_items=100):
     """
     Fetches data from a paginated API endpoint.
+
     Parameters:
-    url (str): The API endpoint URL.
-    headers (dict): Headers for the GitHub API request.
-    max_items (int): Maximum number of items to fetch.
+    - url (str): The API endpoint URL.
+    - headers (dict): Headers for the GitHub API request.
+    - max_items (int): Maximum number of items to fetch.
+
     Returns:
-    list: Aggregated data from all pages.
+    - list: Aggregated data from all pages.
     """
     items = []
     while url and len(items) < max_items:
@@ -41,50 +43,61 @@ def fetch_paginated_data(url, headers, max_items=100):
         else:
             break
     return items[:max_items]
-
-def check_rate_limit(headers):
+def get_repo_info(repo_name, headers):
     """
-    Checks the current rate limit for the GitHub API.
+    Fetches basic information of a specific GitHub repository.
+
     Parameters:
-    headers (dict): Headers for the GitHub API request.
+    - repo_name (str): The full name of the repository (username/repo).
+    - headers (dict): Headers to be used for the GitHub API request.
+
     Returns:
-    tuple: A tuple containing the number of requests remaining and the reset timestamp.
+    - dict: Repository information if successful, None otherwise.
     """
-    rate_limit_url = f"{GITHUB_API_URL}rate_limit"
-    response = requests.get(rate_limit_url, headers=headers)
-    if response.status_code == 200:
-        rate_limit_data = response.json()
-        core = rate_limit_data['resources']['core']
-        search = rate_limit_data['resources']['search']
-        return core['remaining'], core['reset'], search['remaining'], search['reset']
-    else:
-        logging.error(f"Failed to fetch rate limit status: {response.status_code} {response.text}")
-        return 0, None, 0, None
+    repo_url = f'{GITHUB_API_URL}repos/{repo_name}'
+    response = requests.get(repo_url, headers=headers)
+    return handle_api_response(response)
 
-def search_github_code(query, headers):
-    logging.info(f"Checking rate limit status before searching for query: {query}")
-    core_remaining, core_reset, search_remaining, search_reset = check_rate_limit(headers)
+def get_commit_history(repo_name, headers, max_commits=100):
+    """
+    Fetches the commit history of a specific GitHub repository with pagination support.
 
-    if search_remaining > 0:
-        search_url = f"{GITHUB_API_URL}search/code?q={query}"
-        logging.info(f"Sending API request to search code: {search_url}")
-        response = requests.get(search_url, headers=headers)
-        return handle_api_response(response)
-    else:
-        wait_time = max(0, search_reset - int(time.time())) + 10
-        logging.warning(f"Search API rate limit reached. Waiting for {wait_time} seconds until reset.")
-        time.sleep(wait_time)
-        return search_github_code(query, headers)
+    Parameters:
+    - repo_name (str): The full name of the repository (username/repo).
+    - headers (dict): Headers to be used for the GitHub API request.
+    - max_commits (int): Maximum number of commits to return.
 
+    Returns:
+    - list: List of commits if successful, None otherwise.
+    """
+    commits_url = f'{GITHUB_API_URL}repos/{repo_name}/commits'
+    return fetch_paginated_data(commits_url, headers, max_commits)
+
+def search_github_for_repos(query, headers, max_repos=100):
+    """
+    Searches GitHub for repositories based on a query with pagination support.
+
+    Parameters:
+    - query (str): The search query.
+    - headers (dict): Headers to be used for the GitHub API request.
+    - max_repos (int): Maximum number of repositories to return.
+
+    Returns:
+    - list: A list of repository names that match the query.
+    """
+    search_url = f"{GITHUB_API_URL}search/repositories?q={query}&sort=updated&order=desc"
+    return fetch_paginated_data(search_url, headers, max_repos)
 def get_file_contents(repo_name, file_path, headers):
     """
     Fetches the content of a specific file in a repository.
+
     Parameters:
-    repo_name (str): Full name of the repository (username/repo).
-    file_path (str): Path to the file in the repository.
-    headers (dict): Headers for the GitHub API request.
+    - repo_name (str): Full name of the repository (username/repo).
+    - file_path (str): Path to the file in the repository.
+    - headers (dict): Headers for the GitHub API request.
+
     Returns:
-    str: Content of the file, or None if an error occurs.
+    - str: Content of the file, or None if an error occurs.
     """
     file_url = f"{GITHUB_API_URL}repos/{repo_name}/contents/{file_path}"
     response = requests.get(file_url, headers=headers)
@@ -94,40 +107,32 @@ def get_file_contents(repo_name, file_path, headers):
     else:
         return None
 
-def get_repo_info(repo_name, headers):
+def search_files_in_repo(repo_name, query, headers):
     """
-    Fetches basic information of a specific GitHub repository.
+    Searches files in a specific repository based on a query.
+    
     Parameters:
-    repo_name (str): The full name of the repository (username/repo).
-    headers (dict): Headers to be used for the GitHub API request.
+    - repo_name (str): The full name of the repository (username/repo).
+    - query (str): The search query for files.
+    - headers (dict): Headers for the GitHub API request.
+    
     Returns:
-    dict: Repository information if successful, None otherwise.
+    - list: List of files matching the query.
     """
-    repo_url = f'{GITHUB_API_URL}repos/{repo_name}'
-    response = requests.get(repo_url, headers=headers)
+    search_url = f"{GITHUB_API_URL}search/code?q=repo:{repo_name}+{query}"
+    response = requests.get(search_url, headers=headers)
     return handle_api_response(response)
-
-def get_commit_history(repo_name, headers, max_commits=100):
-    """
-    Fetches the commit history of a specific GitHub repository with pagination support.
-    Parameters:
-    repo_name (str): The full name of the repository (username/repo).
-    headers (dict): Headers to be used for the GitHub API request.
-    max_commits (int): Maximum number of commits to return.
-    Returns:
-    list: List of commits if successful, None otherwise.
-    """
-    commits_url = f'{GITHUB_API_URL}repos/{repo_name}/commits'
-    return fetch_paginated_data(commits_url, headers, max_commits)
 
 def get_readme_contents(repo_name, headers):
     """
     Fetches the README content of a specific GitHub repository.
+
     Parameters:
-    repo_name (str): The full name of the repository (username/repo).
-    headers (dict): Headers to be used for the GitHub API request.
+    - repo_name (str): The full name of the repository (username/repo).
+    - headers (dict): Headers to be used for the GitHub API request.
+
     Returns:
-    str: README content if successful, None otherwise.
+    - str: README content if successful, None otherwise.
     """
     readme_url = f'{GITHUB_API_URL}repos/{repo_name}/readme'
     response = requests.get(readme_url, headers=headers)
@@ -137,29 +142,38 @@ def get_readme_contents(repo_name, headers):
     else:
         return None
 
-def fetch_repository_data(repo_name, headers):
+def search_github_code(query, headers):
     """
-    Fetches and aggregates important data from a specific GitHub repository.
+    Searches for code snippets on GitHub based on a query.
+
     Parameters:
-    repo_name (str): The full name of the repository (username/repo).
-    headers (dict): Headers to be used for the GitHub API request.
+    - query (str): The search query.
+    - headers (dict): Headers for the GitHub API request.
+
     Returns:
-    dict: A dictionary containing aggregated data from the repository.
+    - list: A list of code search results.
     """
-    repo_info = get_repo_info(repo_name, headers)
-    commits = get_commit_history(repo_name, headers)
-    readme_content = get_readme_contents(repo_name, headers)
+    search_url = f"{GITHUB_API_URL}search/code?q={query}"
+    response = requests.get(search_url, headers=headers)
+    return handle_api_response(response)
 
-    first_commit_date = commits[-1]['commit']['committer']['date'] if commits else None
-    last_commit_date = commits[0]['commit']['committer']['date'] if commits else None
+def check_rate_limit(headers):
+    """
+    Checks the current rate limit for the GitHub API.
 
-    return {
-        "repo_name": repo_name,
-        "owner": repo_info['owner']['login'] if repo_info else None,
-        "repo_creation_date": repo_info['created_at'] if repo_info else None,
-        "last_commit_date": last_commit_date,
-        "first_commit_date": first_commit_date,
-        "readme_contents": readme_content
-    }
+    Parameters:
+    - headers (dict): Headers for the GitHub API request.
 
+    Returns:
+    - int: The number of requests remaining before hitting the rate limit.
+    """
+    rate_limit_url = f"{GITHUB_API_URL}rate_limit"
+    response = requests.get(rate_limit_url, headers=headers)
+    rate_limit_data = handle_api_response(response)
+    if rate_limit_data:
+        remaining = rate_limit_data['resources']['search']['remaining']
+        logging.debug(f"Search API rate limit remaining: {remaining}")
+        return remaining
+    else:
+        return 0
 
